7b369a5583fd648acf8e478fa3ad1ecc14d8c9de
Eric Chrobak <eric@ericchrobak.com>
2018-04-30 23:48:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if !katz_deli = ""
     puts "The line currently is: #{katz_deli}"
   else
-    puts "The line is currenty empty."
+    puts "The line is currently empty."
   end
 end
 
