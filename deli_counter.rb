28bdc5591edeccc7dc68319298a5b9e3565054ab
Eric Chrobak <eric@ericchrobak.com>
2018-04-30 23:47:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 katz_deli = []
+
+def line(katz_deli)
+  if katz_deli
+    puts "The line currently is: #{katz_deli}"
+  else
+    puts "The line is currenty emtpy."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  number = katz_deli.size + 1
+  puts "Welcome, #{name}. You are number #{number} in line."
+end
+
+def now_serving(katz_deli)
+    puts "Currently serving #{katz_deli[0]}"
+end
