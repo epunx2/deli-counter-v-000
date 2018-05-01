2358c09fc07c1b45ac6698b614d3bb8f388254b6
Eric Chrobak <eric@ericchrobak.com>
2018-05-01 00:09:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
+  katz_deli << name
   number = katz_deli.size
   puts "Welcome, #{name}. You are number #{number} in line."
 end
@@ -26,5 +26,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
+    katz_deli.pop
   end
 end
