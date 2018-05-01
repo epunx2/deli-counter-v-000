0b5cff219f6ad57cf4a5df7fa981e086f35dd2dc
Eric Chrobak <eric@ericchrobak.com>
2018-05-01 00:03:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,14 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.size > 0
-    puts "The line is currently: #{katz_deli}"
-  else
+  if katz_deli.size = 0
     puts "The line is currently empty."
+  else
+    counter = 1
+    katz_deli.each do |e|
+      position = " #{counter}. #{e}"
+    end
+    puts "The line is currently: #{position}"
   end
 end
 
